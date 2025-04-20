449534409042809d401c1f4e0aa17890245dc164
Maggie-maoyiqi <u3629118@connect.hku.hk>
2025-04-20 23:48:50 +0800

    Update shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> generateShells(int size) {
+    vector<int> shells(size);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(0, 1);
 
+    for (int i = 0; i < size; i++) {
+        shells[i] = dis(gen);
+    }
+    return shells;
+}
+int sumShells(const std::vector<int>& shells) {
+    int sum = 0;
+    for (int shell : shells) {
+        sum += shell;
+    }
+    return sum;
+}
+int main() {
+    vector<int> shells = generateShells(9);
+    while(sumShells(shells)<3){
+        shells = generateShells(9);
+    }
+}
