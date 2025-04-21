c4346f4c6e78904cd6ec7c1dddefdb9e65011936
Maggie-maoyiqi <u3629118@connect.hku.hk>
2025-04-21 10:36:29 +0800

    Update shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -2,6 +2,9 @@
 #include<vector>
 #include<random>
 using namespace std;
+//create a shell with 9 shell packages
+//and there should be at least 3 live shells and at most 6
+
 vector<int> generateShells(int size) {
     vector<int> shells(size);
     random_device rd;
@@ -13,6 +16,7 @@ vector<int> generateShells(int size) {
     }
     return shells;
 }
+//randomly generate shell
 int sumShells(const std::vector<int>& shells) {
     int sum = 0;
     for (int shell : shells) {
@@ -20,9 +24,12 @@ int sumShells(const std::vector<int>& shells) {
     }
     return sum;
 }
+//sum the shells for checking 
 int main() {
     vector<int> shells = generateShells(9);
-    while(sumShells(shells)<3){
+    // make sure the shell number is between 3 and 6, and randomly generate it 
+    while(sumShells(shells)<3||sumShells(shells)>6){
         shells = generateShells(9);
+        return 0;
     }
 }
