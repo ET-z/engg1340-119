19ade3a232d2dde514ecadf651f4398ab7c08208
DuskNova <108167233+DuskNova@users.noreply.github.com>
2025-04-29 02:50:31 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,8 +1,9 @@
 #ifndef GAME_H
 #define GAME_H
+#include "dealerAI.h"
 
 int main();
-int game(WINDOW *game_win);
+int game(WINDOW *game_win, DealerAILevel aiLevel); 
 int tutorial1(WINDOW *game_win);
 int tutorial2(WINDOW *game_win);
 int tutorial3(WINDOW *game_win);
