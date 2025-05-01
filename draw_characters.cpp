e45070aab3aad284a8dae19fb5acad49d87732a8
ET-z <zolzayatushig@gmail.com>
2025-05-02 02:53:45 +0800

    Fix

--- draw_characters.cpp
+++ draw_characters.cpp
@@ -16,7 +16,7 @@ void draw_dealer_single(WINDOW *dealer_draw)
 
   // Track progress of player
   ifstream fin;
-  string file = "dealer_animation/10.txt";
+  string file = "10.txt";
   fin.open(file);
 
   if (fin.fail())
