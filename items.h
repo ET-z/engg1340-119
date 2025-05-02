ea4b8a3386d3b04d6ba65812e08c6d35f7477f16
ET-z <zolzayatushig@gmail.com>
2025-05-02 12:43:34 +0800

    PUSH

--- items.h
+++ items.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <ncurses.h>
+#include <utility>
 
 class Player; // Forward declaration since Player class is defined elsewhere
 
@@ -25,9 +26,9 @@ void apple(std::vector<int> &shells, Player &player, const std::vector<std::vect
 // Function to use beer item
 void beer(std::vector<int> &shells, Player &player, const std::vector<std::vector<WINDOW *>> &dealerWindows, const std::vector<std::vector<WINDOW *>> &playerWindows);
 
-void draw_item(WINDOW *item_draw, string item);
+void draw_item(WINDOW *item_draw, std::string item);
 
-void random_items(vector<vector<string>> *inventory, int seed_offset = 0);
-string use_random_item(vector<vector<string>> *inventory);
+void random_items(std::vector<std::vector<std::string>> *inventory, int seed_offset = 0);
+std::pair<int, int> use_random_item(std::vector<std::vector<std::string>> *inventory);
 
 #endif // ITEMS_H
